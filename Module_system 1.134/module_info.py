0c8109dc44226d451005aadce12ef8584f122979
unknown <Nicholas@.(none)>
2011-05-16 19:06:00 -0700

    basic setup

--- Module_system 1.134/module_info.py
+++ Module_system 1.134/module_info.py
@@ -2,5 +2,5 @@
 # Point export_dir to the folder you will be keeping your module
 # Make sure you use forward slashes (/) and NOT backward slashes (\)
 
-export_dir = "../WOTS/Modules/Native/"
+export_dir = "../"
 #export_dir = "C:/Program Files/Mount&Blade/Modules/Native/"
